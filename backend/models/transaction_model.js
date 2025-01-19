5e3524fe6cde2cd00c149393e634429c739b5d4c
satupa <satupalv@gmail.com>
2025-01-19 14:58:40 +0200

    transactions done

--- backend/models/transaction_model.js
+++ backend/models/transaction_model.js
@@ -1,10 +1,36 @@
 const db = require('../database');
 
 const transaction = {
-  getAll: (callback) => {
+  getAll: function (callback) {
     return db.query('SELECT * FROM transaction;', callback);
   },
   // Todo
+
+// Get transaction date
+getTransaction_date: function (transaction_date, callback){
+  return db.query('SELECT * FROM transaction WHERE transaction_date=?', [transaction_date], callback);
+},
+
+// Withdraw money, 20, 40, 50, 100
+withdrawal: function (idbank_account, amountToWithdraw, callback) {
+  return db.query('UPDATE idbank_account SET balance = balance - ? WHERE idbank_account=? AND balance >= ?',
+     [amountToWithdraw, idbank_account, amountToWithdraw], callback);
+},
+
+// Show previous 10 transactions
+showPreviousTransactions: function (transaction, callback){
+  return db.query('SELECT * FROM transaction ORDER BY transaction_date DESC LIMIT 10',
+     [transaction], callback)
+},
+
+// Show next 10 transactions
+showNextTransactions: function (transaction, callback){
+  return db.query('SELECT * FROM transaction ORDER BY transaction_date ASC LIMIT 10', 
+    [transaction], callback)
+},
+
+
+
 };
 
 module.exports = transaction;
