5c3cdbf007999e0c8a94025c1ce778f955904e7d
JansQ-75 <janina7590@hotmail.com>
2025-02-20 00:05:36 +0200

    added update CRUD

--- backend/routes/access_rights.js
+++ backend/routes/access_rights.js
@@ -5,47 +5,60 @@ const { checkAdmin } = require('../authentication');
 
 //get all 
 router.get('/', checkAdmin, function (request, response) {
-    access_rights.getAll(function (err, result) {
-      if (err) {
-        response.json(err);
-      } else {
-        response.json(result);
-      }
-    });
+  access_rights.getAll(function (err, result) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(result);
+    }
   });
+});
 
-  
+
 // get by access id
 router.get('/:idaccess_rights', checkAdmin, function (request, response) {
-      access_rights.getById(request.params.idaccess_rights, function (err, result) {
-        if (err) {
-          response.json(err);
-        } else {
-          response.json(result[0]);
-        }
-      });
-    },
-  );
-
- // add an access right 
- router.post('/', checkAdmin, function (request, response) {
-    access_rights.add(request.body, function (err, result) {
-      if (err) {
-        response.json(err);
-      } else {
-        response.json(result.affectedRows);
-      }
-    });
+  access_rights.getById(request.params.idaccess_rights, function (err, result) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(result[0]);
+    }
   });
+},
+);
 
- // delete 
- router.delete('/:idaccess_rights', checkAdmin, function (request, response) {
-    access_rights.delete(request.params.idaccess_rights, function (err, result) {
-      if (err) {
-        response.json(err);
-      } else {
-        response.json(result.affectedRows);
-      }
-    });
-  }); 
+// update access right
+// Admin only
+router.put('/:idaccess_rights', checkAdmin, function (request, response) {
+  access_rights.update(request.params.idaccess_rights, request.body, function (err, result) {
+    if (err) {
+      response.json(err);
+    }
+    else {
+      response.json('Access right data updated successfully');
+    }
+  })
+});
+
+// add an access right 
+router.post('/', checkAdmin, function (request, response) {
+  access_rights.add(request.body, function (err, result) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(result.affectedRows);
+    }
+  });
+});
+
+// delete 
+router.delete('/:idaccess_rights', checkAdmin, function (request, response) {
+  access_rights.delete(request.params.idaccess_rights, function (err, result) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(result.affectedRows);
+    }
+  });
+});
 module.exports = router;
