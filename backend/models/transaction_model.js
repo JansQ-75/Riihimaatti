20ae517dca7530ae18c7037cbceb820361704210
JansQ-75 <janina7590@hotmail.com>
2025-02-22 22:00:36 +0200

    show transactions by descending order

--- backend/models/transaction_model.js
+++ backend/models/transaction_model.js
@@ -6,53 +6,53 @@ const transaction = {
     return db.query('SELECT * FROM transaction;', callback);
   },
 
-// Get transactions by account id
+  // Get transactions by account id
   transactionsById: function (idbank_account, callback) {
     return db.query(
       ` SELECT t.transaction_date, t.withdrawal, b.bank_account_number, c.cardnumber 
       FROM transaction t
       JOIN bank_account b ON t.idbank_account=b.idbank_account 
       LEFT JOIN card c ON t.idcard=c.idcard 
-      WHERE b.idbank_account=?`,
+      WHERE b.idbank_account=? ORDER BY transaction_date DESC`,
       [idbank_account], callback
     );
   },
 
-// Show transactions, previous ten, next ten
-showTransactions: function (idbank_account, offsetValue, callback) {
-  return db.query('CALL showTransactions (?, ?)',
-    [idbank_account, offsetValue],
-    callback);
-},
+  // Show transactions, previous ten, next ten
+  showTransactions: function (idbank_account, offsetValue, callback) {
+    return db.query('CALL showTransactions (?, ?)',
+      [idbank_account, offsetValue],
+      callback);
+  },
 
-// Do a withdrawal 
-makeWithdrawal: function (account_type, idbank_account, idcard, withdrawal, callback) {
-  if (account_type === "debit") {
+  // Do a withdrawal 
+  makeWithdrawal: function (account_type, idbank_account, idcard, withdrawal, callback) {
+    if (account_type === "debit") {
+      return db.query(
+        'CALL makeDebitWithdrawal (?, ?, ?)',
+        [idbank_account, idcard, withdrawal],
+        callback
+      );
+    } else if (account_type === "credit") {
+      return db.query(
+        'CALL makeCreditWithdrawal (?, ?, ?)',
+        [idbank_account, idcard, withdrawal],
+        callback
+      );
+    }
+  },
+
+  // Delete
+  delete: function (idtransaction, callback) {
     return db.query(
-      'CALL makeDebitWithdrawal (?, ?, ?)',
-      [idbank_account, idcard, withdrawal],
-      callback
+      'DELETE FROM transaction WHERE idtransaction=?',
+      [idtransaction],
+      callback,
     );
-  } else if (account_type === "credit") {
-    return db.query(
-      'CALL makeCreditWithdrawal (?, ?, ?)',
-      [idbank_account, idcard, withdrawal],
-      callback
-    );
-  }
-},
+  },
 
-// Delete
-delete: function (idtransaction, callback) {
-  return db.query(
-    'DELETE FROM transaction WHERE idtransaction=?',
-    [idtransaction],
-    callback,
-  );
-},
-
-// Update
-update: function (idtransaction, transaction_data, callback) {
+  // Update
+  update: function (idtransaction, transaction_data, callback) {
     db.query('SELECT * FROM transaction WHERE idtransaction=?', [idtransaction], (err, result) => {
       if (err) {
         return callback(err);
@@ -80,7 +80,7 @@ update: function (idtransaction, transaction_data, callback) {
         },
       );
     });
-},
+  },
 
 
 };
